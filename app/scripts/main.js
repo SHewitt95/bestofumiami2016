697f65d768ebfab9c8f8382ff656e98df2099245
Sherman Hewitt <shewitt7@umiami.edu>
2016-10-03 00:50:16 -0400

    Added unfixed data for testing, implemented single-pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,22 +8,65 @@ $(document).ready(function() {
     var $grid =  $('.grid');
     var limit = 33;
 
-    var box = '<div class="box my_popup_open"><img src="/images/BestofUM_Logo_White.jpg" /></div>';
+    //var box = '<div class="box my_popup_open"><img src="/images/BestofUM_Logo_White.jpg" /></div>';
 
+    var $box;
+    /*
     for (var i = 0; i < limit; i++) {
       $grid.append(box);
-    }
+    }*/
+
+    bestOfData.forEach(function(d) {
+      $box = $('<div class="box my_popup_open"><img src="/images/' + d["fake photo"] + '.jpg" /></div>');
+
+      $box.data({'address': d['address']});
+      $box.data({'author': d['author']});
+      $box.data({'blurb': d['blurb']});
+      $box.data({'category': d['category']});
+      $box.data({'category section': d['category section']});
+      $box.data({'name': d['name']});
+      $box.data({'phone': d['phone']});
+      $box.data({'photo url': d['photo url']});
+      $box.data({'photographer': d['photographer']});
+      $box.data({'voted': d['voted']});
+      $box.data({'website': d['website']});
+
+      //console.log($box.data());
+
+      $grid.append($box);
+    });
 
     $('.grid').isotope({
        // options
        itemSelector: '.box'
        //layoutMode: 'fitRows'
      });
+
+     //console.log(bestOfData);
+  }
+
+  function makeSinglePage(e) {
+    var that = $(this);
+    var category = $('.category');
+    var categoryname = $('.category-name');
+    var name = $('.name');
+    var blurb = $('.blurb');
+
+    category.text(that.data('category'));
+    categoryname.text(that.data('category section'));
+    name.text(that.data('name'));
+    blurb.text(that.data('blurb'));
   }
 
   makeBoxes();
+  $('.box').click(makeSinglePage);
 
      // Initialize the plugin
-     $('#my_popup').popup();
+     $('#my_popup').popup({
+       color: 'white',
+        opacity: 1,
+        transition: '0.3s',
+        scrolllock: true
+     });
 
 });

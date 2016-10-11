3f009e1d2b85efc2f9b6238e69682c5071d942f8
Sherman Hewitt <shewitt7@umiami.edu>
2016-10-11 19:59:22 -0400

    Added images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,120 +1,116 @@
 $(document).ready(function() {
 
-  function makeBoxes() {
 
-    var $grid =  $('.grid');
-    var limit = 33;
+    function makeBoxes() {
 
-    //var box = '<div class="box my_popup_open"><img src="/images/BestofUM_Logo_White.jpg" /></div>';
+      var $grid =  $('.grid');
+      var $box;
 
-    var $box;
-    /*
-    for (var i = 0; i < limit; i++) {
-      $grid.append(box);
-    }*/
+      bestOfData.forEach(function(d) {
 
-    bestOfData.forEach(function(d) {
-      $box = $('<div class="box my_popup_open"><img src="images/' + d['fake photo'] + '.jpg" /></div>');
+        //if (d['photo url'] != "") {
+          $box = $('<div class="box my_popup_open"><img src="images/' + d['photo url'] + '" /></div>');
 
-      $box.data({'address': d['address']});
-      $box.data({'author': d['author']});
-      $box.data({'blurb': d['blurb']});
-      $box.data({'category': d['category']});
-      $box.data({'category section': d['category section']});
-      $box.data({'name': d['name']});
-      $box.data({'phone': d['phone']});
-      $box.data({'photo url': d['photo url']});
-      $box.data({'photographer': d['photographer']});
-      $box.data({'voted': d['voted']});
-      $box.data({'website': d['website']});
+          $box.data({'address': d['address']});
+          $box.data({'author': d['author']});
+          $box.data({'blurb': d['blurb']});
+          $box.data({'category': d['category']});
+          $box.data({'category section': d['category section']});
+          $box.data({'name': d['name']});
+          $box.data({'phone': d['phone']});
+          $box.data({'photo url': d['photo url']});
+          $box.data({'photographer': d['photographer']});
+          $box.data({'voted': d['voted']});
+          $box.data({'website': d['website']});
 
-      //console.log($box.data());
+          $grid.append($box);
+      //  }
 
-      $grid.append($box);
-    });
 
-    $grid.isotope({
-       // options
-       itemSelector: '.box'
-       //layoutMode: 'fitRows'
-     });
 
-     //console.log(bestOfData);
-  }
+      });
 
-  function makeSinglePage() {
-    var that = $(this);
-    var category = $('.category');
-    var categoryname = $('.category-name');
-    var name = $('.name');
-    var blurb = $('.blurb');
-    var author = $('.author');
-    var photographer = $('.photographer');
-    var address = $('.address');
-    var phoneNumber = $('.phone-number');
+      $grid.isotope({
+         // options
+         itemSelector: '.box',
+         layoutMode: 'fitRows'
+       });
 
-    //category.text(that.data('category'));
-    categoryname.text(that.data('category section'));
-    name.text(that.data('name'));
-    blurb.text(that.data('blurb'));
-    author.text('Words by ' + that.data('author'));
-    photographer.text('Photo by ' + that.data('photographer'));
-    address.text(that.data('address'));
-    phoneNumber.text(that.data('phone'));
-  }
+    }
 
-  function doFilter() {
-    var $grid =  $('.grid');
-    var $that = $(this);
-    var $box = $('.box');
-    var $buttons = $('.sort-box-holder p');
+    function makeSinglePage() {
+      var that = $(this);
+      var photo = $('.popup-img-container');
+      var category = $('.category');
+      var categoryname = $('.category-name');
+      var name = $('.name');
+      var blurb = $('.blurb');
+      var author = $('.author');
+      var photographer = $('.photographer');
+      var address = $('.address');
+      var phoneNumber = $('.phone-number');
 
-    $buttons.css('background-color', '#7CBD9F');
-    $that.css('background-color', '#479A3E');
+      //category.text(that.data('category'));
+      categoryname.text(that.data('category section') + ": ");
+      name.text(that.data('name'));
+      blurb.text(that.data('blurb'));
+      author.text('Words by ' + that.data('author'));
+      photographer.text('Photo by ' + that.data('photographer'));
+      address.text(that.data('address'));
+      phoneNumber.text(that.data('phone'));
+      photo.empty();
+      photo.append('<img src="images/' + that.data('photo url') + '" alt="" />');
+    }
 
-    $grid.isotope({
-      filter: function(i,d) {
-        if ($that.text() == 'Voters\' Choice') {
-          return $(d).data('voted') == 'yes';
+    function doFilter() {
+      var $grid =  $('.grid');
+      var $that = $(this);
+      var $box = $('.box');
+      var $buttons = $('.sort-box-holder p');
 
-        } else if ($that.text() == 'View All') {
-          return true;
+      $buttons.css('background-color', '#7CBD9F');
+      $that.css('background-color', '#479A3E');
 
-        } else {
-          return $that.text() == $(d).data('category');
+      $grid.isotope({
+        filter: function(i,d) {
+          if ($that.text() == 'Voters\' Choice') {
+            return $(d).data('voted') == 'yes';
+
+          } else if ($that.text() == 'View All') {
+            return true;
+
+          } else {
+            return $that.text() == $(d).data('category');
+          }
         }
-      }
-    })
+      })
 
-  }
+    }
 
-  $(window).scroll(function(){
-		if ($(this).scrollTop() > 1200) {
-			$('.backtotop').fadeIn();
-		} else {
-			$('.backtotop').fadeOut();
-		}
-	});
+    $(window).scroll(function(){
+  		if ($(this).scrollTop() > 1200) {
+  			$('.backtotop').fadeIn();
+  		} else {
+  			$('.backtotop').fadeOut();
+  		}
+  	});
 
-	//Click event to scroll to top
-	$('.backtotop').click(function(){
-		$('html, body').animate({scrollTop : 250},800);
-		return false;
-	});
+  	//Click event to scroll to top
+  	$('.backtotop').click(function(){
+  		$('html, body').animate({scrollTop : 250},800);
+  		return false;
+  	});
 
-  makeBoxes();
-  $('.box').click(makeSinglePage);
-  $('.sort-box-holder p').click(doFilter);
+    makeBoxes();
+    $('.box').click(makeSinglePage);
+    $('.sort-box-holder p').click(doFilter);
 
-     // Initialize the plugin
-     $('#my_popup').popup({
-       color: 'white',
-        opacity: 1,
-        transition: '0.3s',
-        scrolllock: true
-     });
-
-     //Check to see if the window is top if not then display button
-
+   // Initialize the popup plugin
+   $('#my_popup').popup({
+     color: 'white',
+      opacity: 1,
+      transition: '0.3s',
+      scrolllock: true
+   });
 
 });
